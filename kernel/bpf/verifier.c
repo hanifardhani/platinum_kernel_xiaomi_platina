ec25a44515e17ed22655e5e30996caf5fc37a914
Jakub Kicinski <jakub.kicinski@netronome.com>
2019-01-22 22:45:23 -0800

    UPSTREAM: bpf: verifier: record original instruction index
    The communication between the verifier and advanced JITs is based
    on instruction indexes.  We have to keep them stable throughout
    the optimizations otherwise referring to a particular instruction
    gets messy quickly.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: Quentin Monnet <quentin.monnet@netronome.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -7766,7 +7766,7 @@ int bpf_check(struct bpf_prog **prog, union bpf_attr *attr,
 {
 	struct bpf_verifier_env *env;
 	struct bpf_verifier_log *log;
-	int ret = -EINVAL;
+	int i, len, ret = -EINVAL;
 	bool is_priv;
 
 	/* no program is valid */
@@ -7781,11 +7781,14 @@ int bpf_check(struct bpf_prog **prog, union bpf_attr *attr,
 		return -ENOMEM;
 	log = &env->log;
 
-	env->insn_aux_data = vzalloc(sizeof(struct bpf_insn_aux_data) *
-				     (*prog)->len);
+	len = (*prog)->len;
+	env->insn_aux_data =
+		vzalloc(len * sizeof(struct bpf_insn_aux_data));
 	ret = -ENOMEM;
 	if (!env->insn_aux_data)
 		goto err_free_env;
+	for (i = 0; i < len; i++)
+		env->insn_aux_data[i].orig_idx = i;
 	env->prog = *prog;
 	env->ops = bpf_verifier_ops[env->prog->type];
 
