cf5066d613ba3c9ea1d8d26c0407349cd742f6c8
RyuujiX <saputradenny712@gmail.com>
2022-08-30 04:12:41 +0700

    drivers: mdss: Add Powersuspend hooks
    Change-Id: I06a82b65f485d1bd22d28360fbc6bf5b777400d4
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- drivers/video/fbdev/msm/mdss_dsi.c
+++ drivers/video/fbdev/msm/mdss_dsi.c
@@ -41,6 +41,10 @@
 #include <linux/state_notifier.h>
 #endif
 
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #define CMDLINE_DSI_CTL_NUM_STRING_LEN 2
 
 /* Master structure to hold all the information about the DSI/panel */
@@ -495,6 +499,9 @@ static int mdss_dsi_panel_power_ctrl(struct mdss_panel_data *pdata,
 #ifdef CONFIG_STATE_NOTIFIER
 		state_suspend();
 #endif
+#ifdef CONFIG_POWERSUSPEND
+ 		set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
+#endif
 		break;
 	case MDSS_PANEL_POWER_ON:
 		if (mdss_dsi_is_panel_on_lp(pdata))
@@ -545,6 +552,9 @@ static int mdss_dsi_panel_power_ctrl(struct mdss_panel_data *pdata,
 #endif
 #ifdef CONFIG_STATE_NOTIFIER
 		state_resume();
+#endif
+#ifdef CONFIG_POWERSUSPEND
+ 		set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
 #endif
 		break;
 	case MDSS_PANEL_POWER_LP1:
