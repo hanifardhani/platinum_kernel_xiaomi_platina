092c7c7a62d92efee6f61a8fd12ef8a9ad874a1b
jamison904 <james.jamison85@gmail.com>
2022-12-30 23:25:35 +0700

    cpufreq: Add & Enable bioshock governor
    Change-Id: I71d120495d02edf3b26aa294f1860d1624e7cfb9

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -533,6 +533,9 @@ extern struct cpufreq_governor cpufreq_gov_sched;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL)
 extern struct cpufreq_governor cpufreq_gov_schedutil;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_schedutil)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
+extern struct cpufreq_governor cpufreq_gov_bioshock;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_bioshock)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
