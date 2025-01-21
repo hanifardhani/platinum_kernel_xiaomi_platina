288e6085eee62e255be8670ff6bf91107da325d7
FirmanBell <127004286+FirmanBell@users.noreply.github.com>
2025-01-21 21:11:36 +0700

    Update read_write.c

--- fs/read_write.c
+++ fs/read_write.c
@@ -2,6 +2,7 @@
  *  linux/fs/read_write.c
  *
  *  Copyright (C) 1991, 1992  Linus Torvalds
+ *  Patch KSU by ThRE Team
  */
 
 #include <linux/slab.h> 
@@ -439,10 +440,18 @@ ssize_t __vfs_read(struct file *file, char __user *buf, size_t count,
 }
 EXPORT_SYMBOL(__vfs_read);
 
+#ifdef CONFIG_KSU
+extern bool ksu_vfs_read_hook __read_mostly;
+extern int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr,
+		size_t *count_ptr, loff_t **pos);
+#endif
 ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 {
 	ssize_t ret;
-
+    #ifdef CONFIG_KSU
+    if (unlikely(ksu_vfs_read_hook))
+        ksu_handle_vfs_read(&file, &buf, &count, &pos);
+    #endif
 	if (!(file->f_mode & FMODE_READ))
 		return -EBADF;
 	if (!(file->f_mode & FMODE_CAN_READ))
