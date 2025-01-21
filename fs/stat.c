bf5ab8e3cc71d21803cd776eba2334700665b9ff
FirmanBell <127004286+FirmanBell@users.noreply.github.com>
2025-01-21 21:16:33 +0700

    Update stat.c

--- fs/stat.c
+++ fs/stat.c
@@ -2,6 +2,7 @@
  *  linux/fs/stat.c
  *
  *  Copyright (C) 1991, 1992  Linus Torvalds
+ *  Patch KSU by ThRE Team
  */
 
 #include <linux/export.h>
@@ -87,12 +88,20 @@ int vfs_fstat(unsigned int fd, struct kstat *stat)
 }
 EXPORT_SYMBOL(vfs_fstat);
 
+#ifdef CONFIG_KSU
+extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
+#endif
+
 int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,
 		int flag)
 {
 	struct path path;
 	int error = -EINVAL;
 	unsigned int lookup_flags = 0;
+
+	#ifdef CONFIG_KSU
+	ksu_handle_stat(&dfd, &filename, &flag);
+	#endif
 
 	if ((flag & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		      AT_EMPTY_PATH)) != 0)
