f2f5903bc864354883e32b75ec8ec7067d14fbfc
hanifardhani <hanif.ardhani98@gmail.com>
2026-08-28 00:40:18 +0700

    Revert "BACKPORT: net: fix __dst_negative_advice() race"
    This reverts commit d4ed127972e3628181250f0c4f0485f5b1f95fde.
    
    Change-Id: I6d7131136d88ff17bb749ca519de060e3587b725

--- net/decnet/dn_route.c
+++ net/decnet/dn_route.c
@@ -303,9 +303,10 @@ static struct dst_entry *dn_dst_check(struct dst_entry *dst, __u32 cookie)
 	return NULL;
 }
 
-static void dn_dst_negative_advice(struct sock *sk, struct dst_entry *dst)
+static struct dst_entry *dn_dst_negative_advice(struct dst_entry *dst)
 {
-	sk_dst_reset(sk);
+	dst_release(dst);
+	return NULL;
 }
 
 static void dn_dst_link_failure(struct sk_buff *skb)
